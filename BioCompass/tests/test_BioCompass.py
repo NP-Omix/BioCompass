d1c12acaf069e96535bf183a93d06b2c9d2f64d0
Guilherme Castelao <guilherme@castelao.net>
2016-11-06 11:36:08 -0800

    Testing find_category_from_product, towards #23.

--- BioCompass/tests/test_BioCompass.py
+++ BioCompass/tests/test_BioCompass.py
@@ -14,3 +14,20 @@ def test_importcli():
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     #assert 'Usage: main [OPTIONS] MGBFILE' in help_result.output
+
+
+import pandas as pd
+from ..BioCompass import find_category_from_product
+def test_find_category_from_product():
+    df = pd.DataFrame(
+            {'product': ['permease', 'bla bla permease bla bla']})
+    out = find_category_from_product(df)
+    assert (out['category'] == 'transporter').all()
+
+def test_find_category_from_product_hypothetical():
+    """ Uncataloged product should return hypothetical
+    """
+    df = pd.DataFrame(
+            {'product': ['ipsisLitteris']})
+    out = find_category_from_product(df)
+    assert (out['category'] == 'hypothetical').all()
