b73a5212caa92612347572a49a020e2974f99a11
Guilherme Castelao <guilherme@castelao.net>
2016-11-06 10:29:31 -0800

    New command line option, download-MIBiG.

--- BioCompass/cli.py
+++ BioCompass/cli.py
@@ -3,6 +3,7 @@
 import click
 
 from .BioCompass import download_hits
+from .BioCompass import download_mibig
 
 @click.group()
 def main():
@@ -17,6 +18,14 @@ def downloadHits(mgbfile, outputdir):
     """ Console script for BioCompass"""
     download_hits(mgbfile, outputdir)
 
+@main.command(name="download-mibig")
+@click.option('--outputdir', default='./', type=click.Path(exists=True),
+        help="Path to save the MIBig genbank files.")
+@click.option('--version', type=unicode, default='1.3',
+        help="Version of MIBiG to download.")
+def downloadMIBiG(outputdir, version):
+    """Download MIBiG gbk database."""
+    download_mibig(outputdir, version=version)
 
 if __name__ == "__main__":
     main()
