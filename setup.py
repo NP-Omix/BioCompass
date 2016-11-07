dce60fb7ea81e35548fb213375f3c66168643f72
Guilherme Castelao <guilherme@castelao.net>
2016-11-06 23:18:36 -0800

    It should be at least Alpha status.

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ setup(
     zip_safe=False,
     keywords='BioCompass',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
